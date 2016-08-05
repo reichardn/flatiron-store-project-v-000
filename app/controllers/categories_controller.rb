0b9a284edfe3544a14a415f98f1c4922e006cdfb
reichardn <reichardn@gmail.com>
2016-08-05 19:41:35 -0400

    finished features:category spec

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,12 @@
 class CategoriesController < ApplicationController
 
+  before_action :set_category!, except: [:create, :index, :new]
+
+
+  private 
+
+  def set_category!
+    @category = Category.find(params[:id])
+  end
+
 end
